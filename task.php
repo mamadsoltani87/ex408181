bf90d2b3ebdacaecd8275702da4e141f24cc423c
mamad <mohammad.mahdi.mms1401@gmail.com>
2025-11-09 00:28:17 -0800

    commit1

---
+++ task.php
@@ -0,0 +1,45 @@
+<?php
+
+abstract class Shape {
+    const UNIT = "cm";
+ 
+    abstract public function area();
+}
+
+class Rectangle extends Shape {
+    public $width;
+    public $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+    public function area() {
+        return $this->width * $this->height;
+    }
+}
+class Circle extends Shape {
+    public $radius;
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function area() {
+        return pi() * pow($this->radius, 2);
+    }
+}
+
+
+$rectangle = new Rectangle(5, 17);
+$circle = new Circle(9);
+
+
+echo "Rectangle Area: " . $rectangle->area() . " " . Shape::UNIT . "<br>";
+echo "Circle Area: " . $circle->area() . " " . Shape::UNIT . "<br>";
+?>
+
+
+
+git config --global user.email "mohammad.mahdi.mms1401@gmail.com"
+git config --global user.name "mamad"   
